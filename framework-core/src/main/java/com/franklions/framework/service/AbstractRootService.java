275e733f12a518544623140cac7ba171ba3f9e7a
flsh <fenglianshuang@jiuan.com>
2019-01-31 13:35:45 +0800

    创建基础开发框架

---
+++ framework-core/src/main/java/com/franklions/framework/service/AbstractRootService.java
@@ -0,0 +1,12 @@
+package com.franklions.framework.service;
+
+/**
+ * @author flsh
+ * @version 1.0
+ * @description
+ * 业务层根基类
+ * @date 2019/1/31
+ * @since Jdk 1.8
+ */
+public abstract class AbstractRootService {
+}
