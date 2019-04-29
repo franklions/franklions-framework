17f9f327977133bc7d2210dcf4de97f3dc850958
franklions <franklions@sina.com>
2019-04-29 10:20:15 +0800

    添加配置中心

---
+++ framework-example/src/main/java/com/franklions/example/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.franklions.example.config;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author flsh
+ * @version 1.0
+ * @date 2019-04-08
+ * @since Jdk 1.8
+ */
+@Configuration
+@EnableEurekaClient
+public class AppConfig {
+
+}
