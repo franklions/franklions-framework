f785c0d5866fffad910bcb1a564c422d445100f9
冯连爽 <fenglianshuang@lianwukeji.com>
2021-08-17 11:21:13 +0800

    添加单元测试代码 修复数据库版本问题

--- src/test/java/com/franklions/example/controller/UserControllerTest.java
+++ src/test/java/com/franklions/example/controller/UserControllerTest.java
@@ -1,76 +1,78 @@
-//package com.franklions.example.controller;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
-//import org.springframework.mock.web.MockHttpServletResponse;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import javax.sql.DataSource;
-//import java.util.Optional;
-//
-//import static org.junit.Assert.*;
-//import static org.mockito.Matchers.anyString;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//
-///**
-// * @author flsh
-// * @version 1.0
-// * @description
-// * @date 2019/2/13
-// * @since Jdk 1.8
-// */
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(UserController.class)
-//public class UserControllerTest {
-//
-//    @MockBean
-//    DataSource dataSource;
-//
-//    @MockBean
-//    UserService userService;
-//
-//    @Autowired
-//    MockMvc mockMvc;
-//
-//    ObjectMapper objectMapper;
-//    UserDTO user;
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        objectMapper = new ObjectMapper();
-//        user = new UserDTO();
-//    }
-//
-//    @Test
-//    public void testGetAllUser() throws Exception {
-//    }
-//
-//    @Test
-//    public void testGetUserByName() throws Exception {
-//    }
-//
-//    @Test
-//    public void testGetUserByAccount() throws Exception {
-//        when(this.userService.getUserByAccount(anyString())).thenReturn(Optional.ofNullable(user));
-//
-//        MockHttpServletResponse response = this.mockMvc.perform(get("/api/user/acc")
-//                .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                .accept(MediaType.APPLICATION_JSON_UTF8))
-//                .andReturn().getResponse();
-//
-//        System.out.println("response:"+response.getContentAsString());
-//        assertEquals(HttpStatus.OK.value(),response.getStatus());
-//        verify(this.userService).getUserByAccount(anyString());
-//    }
-//
-//}
+package com.franklions.example.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.franklions.example.domain.dto.UserDTO;
+import com.franklions.example.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.sql.DataSource;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+/**
+ * @author flsh
+ * @version 1.0
+ * @description
+ * @date 2019/2/13
+ * @since Jdk 1.8
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @MockBean
+    DataSource dataSource;
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper;
+    UserDTO user;
+
+    @Before
+    public void setUp() throws Exception {
+        objectMapper = new ObjectMapper();
+        user = new UserDTO();
+    }
+
+    @Test
+    public void testGetAllUser() throws Exception {
+    }
+
+    @Test
+    public void testGetUserByName() throws Exception {
+    }
+
+    @Test
+    public void testGetUserByAccount() throws Exception {
+        when(this.userService.getUserByAccount(anyString())).thenReturn(Optional.ofNullable(user));
+
+        MockHttpServletResponse response = this.mockMvc.perform(get("/api/user/acc")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andReturn().getResponse();
+
+        System.out.println("response:"+response.getContentAsString());
+        assertEquals(HttpStatus.OK.value(),response.getStatus());
+        verify(this.userService).getUserByAccount(anyString());
+    }
+
+}
--- src/test/java/com/franklions/example/service/UserServiceTest.java
+++ src/test/java/com/franklions/example/service/UserServiceTest.java
@@ -1,73 +1,76 @@
-//package com.franklions.example.service;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertNotNull;
-//import static org.mockito.Mockito.*;
-//
-///**
-// * @author flsh
-// * @version 1.0
-// * @description
-// * @date 2019/2/13
-// * @since Jdk 1.8
-// */
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class UserServiceTest {
-//
-//    @MockBean
-//    UserMapper userMapper;
-//
-//    @Autowired
-//    UserService userService;
-//
-//    UserDO user;
-//
-//    @Before
-//    public void initData(){
-//        user = new UserDO();
-//        user.setId(1);
-//        user.setAccount("admin");
-//        user.setName("管理员");
-//    }
-//
-//    @Test
-//    public void getAllUsers() throws Exception {
-//        List<UserDO> allUsers = new ArrayList<UserDO>(){{add(user);add(user);add(user);add(user);add(user);}};
-//        when(this.userMapper.selectAll()).thenReturn(allUsers);
-//        List<UserDTO> result = this.userService.getAllUsers();
-//        assertEquals(result.get(0).getId(), allUsers.get(0).getId());
-//        assertNotNull(result);
-//        verify(this.userMapper,times(1)).selectAll();
-//    }
-//
-//    @Test
-//    public void getUserByName() throws Exception {
-//        List<UserDO> allUsers = new ArrayList<UserDO>(){{add(user);add(user);add(user);add(user);add(user);}};
-//        when(this.userMapper.selectByName(anyString())).thenReturn(allUsers);
-//        List<UserDTO> result = this.userService.getUserByName("管理员");
-//        assertNotNull(result);
-//        verify(this.userMapper,times(1)).selectByName(anyString());
-//    }
-//
-//    @Test
-//    public void getUserByAccount() throws Exception {
-//        when(this.userMapper.selectByAccount(anyString())).thenReturn(user);
-//         Optional<UserDTO> userDTO = this.userService.getUserByAccount("admin");
-//        assertNotNull(userDTO);
-//        verify(this.userMapper,times(1)).selectByAccount(anyString());
-//    }
-//
-//}
+package com.franklions.example.service;
+
+import com.franklions.example.domain.dto.UserDTO;
+import com.franklions.example.domain.entity.UserDO;
+import com.franklions.example.mapper.UserMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author flsh
+ * @version 1.0
+ * @description
+ * @date 2019/2/13
+ * @since Jdk 1.8
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTest {
+
+    @MockBean
+    UserMapper userMapper;
+
+    @Autowired
+    UserService userService;
+
+    UserDO user;
+
+    @Before
+    public void initData(){
+        user = new UserDO();
+        user.setId(1);
+        user.setAccount("admin");
+        user.setName("管理员");
+    }
+
+    @Test
+    public void getAllUsers() throws Exception {
+        List<UserDO> allUsers = new ArrayList<UserDO>(){{add(user);add(user);add(user);add(user);add(user);}};
+        when(this.userMapper.selectAll()).thenReturn(allUsers);
+        List<UserDTO> result = this.userService.getAllUsers();
+        assertEquals(result.get(0).getId(), allUsers.get(0).getId());
+        assertNotNull(result);
+        verify(this.userMapper,times(1)).selectAll();
+    }
+
+    @Test
+    public void getUserByName() throws Exception {
+        List<UserDO> allUsers = new ArrayList<UserDO>(){{add(user);add(user);add(user);add(user);add(user);}};
+        when(this.userMapper.selectByName(anyString())).thenReturn(allUsers);
+        List<UserDTO> result = this.userService.getUserByName("管理员");
+        assertNotNull(result);
+        verify(this.userMapper,times(1)).selectByName(anyString());
+    }
+
+    @Test
+    public void getUserByAccount() throws Exception {
+        when(this.userMapper.selectByAccount(anyString())).thenReturn(user);
+         Optional<UserDTO> userDTO = this.userService.getUserByAccount("admin");
+        assertNotNull(userDTO);
+        verify(this.userMapper,times(1)).selectByAccount(anyString());
+    }
+
+}
