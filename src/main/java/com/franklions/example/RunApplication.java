b67dbb02025dec0bfbc3e274f602ba948ae3f027
冯连爽 <fenglianshuang@lianwukeji.com>
2021-08-24 09:04:08 +0800

    修改应用启动bug

--- src/main/java/com/franklions/example/RunApplication.java
+++ src/main/java/com/franklions/example/RunApplication.java
@@ -13,6 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RunApplication {
     public static void main(String[] args) {
-        SpringApplication.run(RunApplication.class);
+        SpringApplication.run(RunApplication.class,args);
     }
 }
