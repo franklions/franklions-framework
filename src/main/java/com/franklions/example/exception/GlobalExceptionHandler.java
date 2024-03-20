097b233f0eb36144d87e079e5c28a82ab75a3acf
冯连爽 <fenglianshuang@lianwukeji.com>
2024-03-20 21:42:55 +0800

    修改全局异常处理

--- src/main/java/com/franklions/example/exception/GlobalExceptionHandler.java
+++ src/main/java/com/franklions/example/exception/GlobalExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.UnsatisfiedServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -85,6 +86,16 @@ public class GlobalExceptionHandler {
         return new ErrorResult(400000,"缺少请求参数");
     }
 
+
+    /**
+     * 400 - Bad Request
+     */
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ErrorResult handleMissingRequestHeaderException(MissingRequestHeaderException e) {
+        logger.warn("缺少请求头参数", e);
+        return new ErrorResult(400000,"缺少请求头参数");
+    }
 
     /**
      * 400 - Bad Request
